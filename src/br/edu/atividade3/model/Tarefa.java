b8afd9863187fda576917082c599802109e5ff82
Juan Carlos Leite da Silva <juancarlosleitedasilva@gmail.com>
2026-01-07 15:35:05 -0300

    Finalizando a classe Tarefa, adicionando os atributos, criando um construtor, criando os métodos getters e setters e sobrescrevendo o método toString

--- src/br/edu/atividade3/model/Tarefa.java
+++ src/br/edu/atividade3/model/Tarefa.java
@@ -1,5 +1,44 @@
 package br.edu.atividade3.model;
 
 public class Tarefa {
+    private String nome;
+    private String descricao;
+    private Status status;
+
+    public Tarefa(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.status = Status.DISPONIVEL;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s %s", status, nome, descricao);
+    }
+
 
 }
