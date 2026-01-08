ff1ee42acc33a898a8e9fd3710cfbb8e533b14f7
gabrielmarinho1 <gabriel.marinho112@gmail.com>
2026-01-07 22:21:54 -0300

    correcao do comentario singleton

--- src/br/edu/atividade3/model/TarefaRepository.java
+++ src/br/edu/atividade3/model/TarefaRepository.java
@@ -3,9 +3,9 @@ package br.edu.atividade3.model;
 import java.util.ArrayList;
 import java.util.List;
 
-/*singleton para que o programa gerencie sempre a mesma lista de tarefas como uma array */
-
 public class TarefaRepository {
+
+    /*singleton para que o programa gerencie sempre a mesma lista de tarefas como uma array */
     private static TarefaRepository instance;
     private List<Tarefa> tarefas;
 
